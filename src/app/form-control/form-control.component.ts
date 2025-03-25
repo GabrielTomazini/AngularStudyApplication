3321dfde28215d48d1c3201d2d001e1bac10adb4
Gabriel Tomazini Marani <gabrieltm.ifms@gmail.com>
2025-03-25 17:08:58 -0300

    Reactive Forms Study

--- src/app/form-control/form-control.component.ts
+++ src/app/form-control/form-control.component.ts
@@ -1,27 +1,24 @@
 import { Component, inject } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import {
   ReactiveFormsModule,
-  FormGroup,
+  FormBuilder,
+  Validators,
   FormsModule,
-  FormControl,
+  FormArray,
 } from '@angular/forms';
 
 @Component({
   selector: 'app-form-control',
   standalone: true,
-  imports: [
-    FormsModule,
-    ReactiveFormsModule,
-  ],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './form-control.component.html',
-  styleUrl: './form-control.component.scss'
+  styleUrl: './form-control.component.scss',
 })
 export class FormControlComponent {
-
   private formBuilder = inject(FormBuilder);
   profileForm = this.formBuilder.group({
-    firstName: [''],
+    firstName: ['', Validators.required],
     lastName: [''],
     address: this.formBuilder.group({
       street: [''],
@@ -29,8 +26,16 @@ export class FormControlComponent {
       state: [''],
       zip: [''],
     }),
-
+    aliases: this.formBuilder.array([this.formBuilder.control('')]),
   });
+
+  get aliases() {
+    return this.profileForm.get('aliases') as FormArray;
+  }
+
+  addAlias() {
+    this.aliases.push(this.formBuilder.control(''));
+  }
 
   updateProfile() {
     this.profileForm.patchValue({
